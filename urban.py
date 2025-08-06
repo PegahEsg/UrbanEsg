3b660d3b411f16e591bd215ade15c758dc15535a
PegahEsg <130217438+PegahEsg@users.noreply.github.com>
2025-08-06 11:16:38 +0330

    Update urban.py

--- urban.py
+++ urban.py
@@ -1561,13 +1561,13 @@ if on:
         with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
             download.to_excel(writer, sheet_name='Sheet1')
         
-        st.download_button(
-            label="Download optimization results",
-            data=buffer.getvalue(),
-            file_name="optimization_results.xlsx",
-            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
-
+st.download_button(
+    label="Download optimization results",
+    data=buffer.getvalue(),
+    file_name="optimization_results.xlsx",
+    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    key="unique_download_button"
+)
 
 
     
