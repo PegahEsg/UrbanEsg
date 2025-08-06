374cd778adf6e89d328299e633fae882d79d06d3
PegahEsg <130217438+PegahEsg@users.noreply.github.com>
2025-08-06 11:50:29 +0330

    Update urban.py

--- urban.py
+++ urban.py
@@ -283,9 +283,6 @@ my_list=[EUI,Cooling,Heating,Lighting,roof_hot,Hours,roof_cold,SVF,Visibility,PV
 for i in range(len(my_list)):
     if my_list[i] == True:
         true_indexes.append(i)
-        
-#options=int(st.sidebar.number_input("How many Alternative do you want?",min_value=1,max_value=5,value=1,step=1))
-
 
 my_list1 = ["EUI","Cooling","Heating","Lighting","Radiation-Hottest week","Solar Hours","Radiation-Coldest week","SVF","Visibility","PV Power","CO2 Emission"]
 
@@ -311,45 +308,17 @@ for metric in selected_metrics:
     weights[metric] = st.sidebar.slider(f"Weight for {metric}", 1.0, 10.0, 1.0, 1.0)
 
 
-on = st.sidebar.button('Optimize') 
 
 def calculate_weighted_score(solution, selected_metrics, weights, my_list1, true_indexes):
-    metric_ranges = {
-        'EUI': (80, 400),
-        'Cooling': (70, 160),
-        'Heating': (40, 100),
-        'Lighting': (15, 40),
-        'PV Power': (0.0, 0.5),
-        'CO2 Emission': (0, 1000),
-        'Solar Hours': (0, 5),
-        'Radiation-Coldest week': (50, 1000),
-        'Radiation-Hottest week': (100, 1000),
-        'SVF': (30, 100),
-        'Visibility': (30, 100)
-    }
-
     score = 0
-    total_weight = 0
-
     for i in range(len(true_indexes)):
         metric_name = my_list1[true_indexes[i]]
-
-        if metric_name not in selected_metrics:
-            continue  # Only include selected metrics
-
-        value = solution.objectives[i]
         weight = weights.get(metric_name, 1)
+        score += solution.objectives[i] * weight
+    return score
 
-        min_val, max_val = metric_ranges.get(metric_name, (0, 1))
-        normalized = (value - min_val) / (max_val - min_val)
-        normalized = max(0, min(normalized, 1))  # Clamp between 0 and 1
-
-        score += normalized * weight
-        total_weight += weight
-
-    final_score = (score / total_weight) * 10 if total_weight > 0 else 0
-    return round(final_score, 2)
-
+options=int(st.sidebar.number_input("How many Alternative do you want?",min_value=1,max_value=5,value=1,step=1))
+on = st.sidebar.button('Optimize')  
 def neigbors_h(stories,park_loc,u,v):
     def add_padding(matrix):
         padded_matrix = [[0] * (len(matrix[0]) + 2)]  # Adding top padding
@@ -877,37 +846,39 @@ def optimize(input):
     return Objectives 
     
 def X_var():
-    var0 = Subset([-45, 0, 45], 1)  # Rotation
-    var1 = Subset([6, 12], 1)       # Street width
-    var2 = Subset([0.45, 0.6], 1)   # Building Footprint
-    var3 = Subset([1, 1], 1)        # WWR
+    
+    
+    var0 = Subset([-45,0,45],1) #Rotation
+    var1 = Subset([6,12],1) #Street width
+    var2 = Subset([0.45,0.6],1) #Building Footprint
+    var3 = Subset([1,1],1) #WWR
+           
+    
+    
+    var4 = Subset(2*(n_parcel - round(n_parcel * green_ratio))*[4,5],(n_parcel - round(n_parcel * green_ratio))*2) #Stories
+    var5 = Subset(2*(n_parcel - round(n_parcel * green_ratio))*[4,5,6],(n_parcel - round(n_parcel * green_ratio))*2) #Stories
+    var6 = Subset(2*(n_parcel - round(n_parcel * green_ratio))*[5,6,7],(n_parcel - round(n_parcel * green_ratio))*2) #Stories
+    var7 = Subset(2*(n_parcel - round(n_parcel * green_ratio))*[6,7,8],(n_parcel - round(n_parcel * green_ratio))*2) #Stories
+    var8 = Subset(2*(n_parcel - round(n_parcel * green_ratio))*[7,8,9],(n_parcel - round(n_parcel * green_ratio))*2) #Stories
+    var9 = Subset(2*(n_parcel - round(n_parcel * green_ratio))*[8,9,10],(n_parcel - round(n_parcel * green_ratio))*2) #Stories
+    
+    var10 = Subset(list(range(n_parcel)),n_parcel - round(n_parcel * green_ratio))
+    var11=Subset([1,2,3]*len(one_nim)*2,len(one_nim)*2) #Com_floor_vaghti nesfe mahale edarie
+    var12=Subset([0.5,0.7],1) #res_ratio
 
-    var4 = Subset(2 * (n_parcel - round(n_parcel * green_ratio)) * [4, 5], (n_parcel - round(n_parcel * green_ratio)) * 2)
-    var5 = Subset(2 * (n_parcel - round(n_parcel * green_ratio)) * [4, 5, 6], (n_parcel - round(n_parcel * green_ratio)) * 2)
-    var6 = Subset(2 * (n_parcel - round(n_parcel * green_ratio)) * [5, 6, 7], (n_parcel - round(n_parcel * green_ratio)) * 2)
-    var7 = Subset(2 * (n_parcel - round(n_parcel * green_ratio)) * [6, 7, 8], (n_parcel - round(n_parcel * green_ratio)) * 2)
-    var8 = Subset(2 * (n_parcel - round(n_parcel * green_ratio)) * [7, 8, 9], (n_parcel - round(n_parcel * green_ratio)) * 2)
-    var9 = Subset(2 * (n_parcel - round(n_parcel * green_ratio)) * [8, 9, 10], (n_parcel - round(n_parcel * green_ratio)) * 2)
-
-    var10 = Subset(list(range(n_parcel)), n_parcel - round(n_parcel * green_ratio))
-    var11 = Subset([1, 2, 3] * len(one_nim) * 2, len(one_nim) * 2)  # Com_floor_vaghti nesfe mahale edarie
-    var12 = Subset([0.5, 0.7], 1)  # res_ratio
-
-    var13 = Permutation(zero_nim + one_nim)
-    var14 = Permutation(zero_haf + one_haf)
-
-    var15 = Subset([0.1, 0.2, 0.3], 1)  # com_ratio
-    var16 = Subset([1, 2, 3] * len(one_nim) * 2, len(one_haf) * 2)  # Com_floor_vaghti 0.3 mahale edarie
-
-    return var0, var1, var2, var3, var4, var5, var6, var7, var8, var9, var10, var11, var12, var13, var14, var15, var16
-
-
-
-if on:
+    var13=Permutation((zero_nim+one_nim))
+    var14=Permutation((zero_haf+one_haf))
+    
+    var15=Subset([0.1,0.2,0.3],1) #com_ratio
+    var16=Subset([1,2,3]*len(one_nim)*2,len(one_haf)*2) #Com_floor_vaghti 0.3 mahale edarie
+    return var0,var1,var2,var3,var4,var5,var6,var7,var8,var9,var10,var11,var12,var13,var14,var15,var16
+        
+if on:    
     n_park = n_park_f(n_parcel)
     parcel_list = list(range(n_parcel))
     park_parcels = np.sort(np.random.choice(parcel_list, size=n_park, replace=False))
     building_parcels = [i for i in parcel_list if i not in park_parcels]
+
     parcels_X = []
     #arman
     for i in range(u):
@@ -1585,7 +1556,7 @@ if on:
         
                 
         st.divider()
-          
+        
     with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
         download.to_excel(writer, sheet_name='Sheet1')    
         # Close the Pandas Excel writer and output the Excel file to the buffer
