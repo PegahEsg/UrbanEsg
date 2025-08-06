3568ffd87f50a6057c62c0b339ae69402aef1e11
PegahEsg <130217438+PegahEsg@users.noreply.github.com>
2025-08-06 09:19:26 +0330

    Update urban.py

--- urban.py
+++ urban.py
@@ -305,7 +305,7 @@ selected_metrics = [my_list1[i] for i in true_indexes]
 st.sidebar.markdown("### Weight assignment to selected metrics")
 weights = {}
 for metric in selected_metrics:
-    weights[metric] = st.sidebar.slider(f"Weight for {metric}", 0.0, 10.0, 1.0, 1.0)
+    weights[metric] = st.sidebar.slider(f"Weight for {metric}", 1.0, 10.0, 1.0, 1.0)
 
 
 options=int(st.sidebar.number_input("How many Alternative do you want?",min_value=1,max_value=5,value=1,step=1))
